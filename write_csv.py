455863d405977472b8f7a995529379106b2e2e91
mmvih <mmvihani@gmail.com>
2021-12-06 13:35:55 -0700

    uses the right encoder base

--- write_csv.py
+++ write_csv.py
@@ -17,6 +17,150 @@ import concurrent.futures
 
 available_gpus = [torch.cuda.device(i) for i in range(torch.cuda.device_count())]
 
+ENCODERS = {
+    'ResNet': {
+        'resnet18': ['imagenet', 'ssl', 'swsl'],
+        'resnet34': ['imagenet'],
+        'resnet50': ['imagenet', 'ssl', 'swsl'],
+        'resnet101': ['imagenet'],
+        'resnet152': ['imagenet'],
+    },
+    'ResNeXt': {
+        'resnext50_32x4d': ['imagenet', 'ssl', 'swsl'],
+        'resnext101_32x4d': ['ssl', 'swsl'],
+        'resnext101_32x8d': ['imagenet', 'instagram', 'ssl', 'swsl'],
+        'resnext101_32x16d': ['instagram', 'ssl', 'swsl'],
+        'resnext101_32x32d': ['instagram'],
+        'resnext101_32x48d': ['instagram'],
+    },
+    'ResNeSt': {
+        'timm-resnest14d': ['imagenet'],
+        'timm-resnest26d': ['imagenet'],
+        'timm-resnest50d': ['imagenet'],
+        'timm-resnest101e': ['imagenet'],
+        'timm-resnest200e': ['imagenet'],
+        'timm-resnest269e': ['imagenet'],
+        'timm-resnest50d_4s2x40d': ['imagenet'],
+        'timm-resnest50d_1s4x24d': ['imagenet'],
+    },
+    'Res2Ne(X)t': {
+        'timm-res2net50_26w_4s': ['imagenet'],
+        'timm-res2net101_26w_4s': ['imagenet'],
+        'timm-res2net50_26w_6s': ['imagenet'],
+        'timm-res2net50_26w_8s': ['imagenet'],
+        'timm-res2net50_48w_2s': ['imagenet'],
+        'timm-res2net50_14w_8s': ['imagenet'],
+        'timm-res2next50': ['imagenet'],
+    },
+    'RegNet(x/y)': {
+        'timm-regnetx_002': ['imagenet'],
+        'timm-regnetx_004': ['imagenet'],
+        'timm-regnetx_006': ['imagenet'],
+        'timm-regnetx_008': ['imagenet'],
+        'timm-regnetx_016': ['imagenet'],
+        'timm-regnetx_032': ['imagenet'],
+        'timm-regnetx_040': ['imagenet'],
+        'timm-regnetx_064': ['imagenet'],
+        'timm-regnetx_080': ['imagenet'],
+        'timm-regnetx_120': ['imagenet'],
+        'timm-regnetx_160': ['imagenet'],
+        'timm-regnetx_320': ['imagenet'],
+        'timm-regnety_002': ['imagenet'],
+        'timm-regnety_004': ['imagenet'],
+        'timm-regnety_006': ['imagenet'],
+        'timm-regnety_008': ['imagenet'],
+        'timm-regnety_016': ['imagenet'],
+        'timm-regnety_032': ['imagenet'],
+        'timm-regnety_040': ['imagenet'],
+        'timm-regnety_064': ['imagenet'],
+        'timm-regnety_080': ['imagenet'],
+        'timm-regnety_120': ['imagenet'],
+        'timm-regnety_160': ['imagenet'],
+        'timm-regnety_320': ['imagenet'],
+    },
+    'GERNet': {
+        'timm-gernet_s': ['imagenet'],
+        'timm-gernet_m': ['imagenet'],
+        'timm-gernet_l': ['imagenet'],
+    },
+    'SE-Net': {
+        'senet154': ['imagenet'],
+        'se_resnet50': ['imagenet'],
+        'se_resnet101': ['imagenet'],
+        'se_resnet152': ['imagenet'],
+        'se_resnext50_32x4d': ['imagenet'],
+        'se_resnext101_32x4d': ['imagenet'],
+    },
+    'SK-ResNe(X)t': {
+        'timm-skresnet18': ['imagenet'],
+        'timm-skresnet34': ['imagenet'],
+        'timm-skresnext50_32x4d': ['imagenet'],
+    },
+    'DenseNet': {
+        'densenet121': ['imagenet'],
+        'densenet169': ['imagenet'],
+        'densenet201': ['imagenet'],
+        'densenet161': ['imagenet'],
+    },
+    'Inception': {
+        'inceptionresnetv2': ['imagenet', 'imagenet+background'],
+        'inceptionv4': ['imagenet', 'imagenet+background'],
+        'xception': ['imagenet'],
+    },
+    'EfficientNet': {
+        'efficientnet-b0': ['imagenet'],
+        'efficientnet-b1': ['imagenet'],
+        'efficientnet-b2': ['imagenet'],
+        'efficientnet-b3': ['imagenet'],
+        'efficientnet-b4': ['imagenet'],
+        'efficientnet-b5': ['imagenet'],
+        'efficientnet-b6': ['imagenet'],
+        'efficientnet-b7': ['imagenet'],
+        'timm-efficientnet-b0': ['imagenet', 'advprop', 'noisy-student'],
+        'timm-efficientnet-b1': ['imagenet', 'advprop', 'noisy-student'],
+        'timm-efficientnet-b2': ['imagenet', 'advprop', 'noisy-student'],
+        'timm-efficientnet-b3': ['imagenet', 'advprop', 'noisy-student'],
+        'timm-efficientnet-b4': ['imagenet', 'advprop', 'noisy-student'],
+        'timm-efficientnet-b5': ['imagenet', 'advprop', 'noisy-student'],
+        'timm-efficientnet-b6': ['imagenet', 'advprop', 'noisy-student'],
+        'timm-efficientnet-b7': ['imagenet', 'advprop', 'noisy-student'],
+        'timm-efficientnet-b8': ['imagenet', 'advprop'],
+        'timm-efficientnet-l2': ['noisy-student'],
+        'timm-efficientnet-lite0': ['imagenet'],
+        'timm-efficientnet-lite1': ['imagenet'],
+        'timm-efficientnet-lite2': ['imagenet'],
+        'timm-efficientnet-lite3': ['imagenet'],
+        'timm-efficientnet-lite4': ['imagenet'],
+    },
+    'MobileNet': {
+        'mobilenet_v2': ['imagenet'],
+        'timm-mobilenetv3_large_075': ['imagenet'],
+        'timm-mobilenetv3_large_100': ['imagenet'],
+        'timm-mobilenetv3_large_minimal_100': ['imagenet'],
+        'timm-mobilenetv3_small_075': ['imagenet'],
+        'timm-mobilenetv3_small_100': ['imagenet'],
+        'timm-mobilenetv3_small_minimal_100': ['imagenet'],
+    },
+    'DPN': {
+        'dpn68': ['imagenet'],
+        'dpn68b': ['imagenet+5k'],
+        'dpn92': ['imagenet+5k'],
+        'dpn98': ['imagenet'],
+        'dpn107': ['imagenet+5k'],
+        'dpn131': ['imagenet'],
+    },
+    'VGG': {
+        'vgg11': ['imagenet'],
+        'vgg11_bn': ['imagenet'],
+        'vgg13': ['imagenet'],
+        'vgg13_bn': ['imagenet'],
+        'vgg16': ['imagenet'],
+        'vgg16_bn': ['imagenet'],
+        'vgg19': ['imagenet'],
+        'vgg19_bn': ['imagenet'],
+    },
+}
+
 modelName = ['Unet', 'UnetPlusPlus', 'MAnet', 'Linknet', 'FPN', 'PSPNet', 'PAN', 'DeepLabV3', 'DeepLabV3Plus'] #9
 lossName = ['MCCLoss'] #, 'JaccardLoss', 'DiceLoss', 'TverskyLoss', 'FocalLoss', 'LovaszLoss', 'SoftBCEWithLogitsLoss', 'SoftCrossEntropyLoss'] #1
 encoderVariant = ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152', "resnext50_32x4d", "resnext101_32x4d", 
@@ -51,6 +195,7 @@ validablumentations = ["NA"] #1
 arguments = {
     "modelName":modelName,
     "lossName":lossName, 
+    # "encoderBase":list(ENCODERS.keys()),
     "encoderVariant":encoderVariant,
     "encoderWeights":encoderWeights,
     "optimizerName":optimizerName,
@@ -85,6 +230,7 @@ def main():
         keys   = list(arguments.keys())
         iter_combos = itertools.product(*values)
         
+
         with open(csv_path, 'w') as csvFile:
             writer = csv.writer(csvFile)
             writer.writerow(keys)
