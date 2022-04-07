4d3679eb1472b689d1523cd34bef5ea77422dde4
mmvih <mmvihani@gmail.com>
2022-04-07 16:21:02 -0600

    runs with multiple GPUS

--- pixelEvalAnalysis.py
+++ pixelEvalAnalysis.py
@@ -12,7 +12,9 @@ import tempfile
 import torch
 import torchvision
 
+from multiprocessing import Pool, current_process, Queue 
 import subprocess
+import concurrent.futures
 
 import bfio
 from bfio import BioWriter
@@ -29,20 +31,20 @@ from src.utils import LOSSES
 
 """INPUT PARAMETERS"""
 
-smp_inputs_path =  "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/dummy_output_SMP/"
-smp_outputs_path = "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/dummy_output_SMP_pixeleval/"
+smp_inputs_path =  "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/output_SMP/"
+smp_outputs_path = "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/output_SMP_pixeleval/"
 
 testing_images = "/home/vihanimm/SegmentationModelToolkit/Data/tif_data/nuclear/test/image/"
 testing_labels = "/home/vihanimm/SegmentationModelToolkit/Data/tif_data/nuclear/test/groundtruth_centerbinary_2pixelsmaller/"
 
 smp_inputs_list = os.listdir(smp_inputs_path)
 
-"""GETTING THE LOADERS"""
+available_gpus = [torch.cuda.device(i) for i in range(torch.cuda.device_count())]       
 
 def getLoader(images_Dir, 
               labels_Dir):
     
-    filepattern = "nuclear_test_60{x}.tif"
+    filepattern = ".*"
     images_fp = FilePattern(testing_images, filepattern)
     labels_fp = FilePattern(testing_labels, filepattern)
 
@@ -60,93 +62,105 @@ def getLoader(images_Dir,
         
     return testing_dataset, testing_dataset_vis, images_fp, labels_fp
 
-print("Getting Loaders")
-test_loader, test_loader_vis, images_fp, labels_fp = getLoader(images_Dir=testing_images,
-                                            labels_Dir=testing_labels)
-print("Done with Loaders")
-test_loader_len = torch.tensor(len(test_loader))
+
 
 """MAKING PREDICTIONS"""
 
-tor_device = torch.device("cuda:0")
-for smp_model in smp_inputs_list:
+queue = Queue()
+def evaluation(smp_model : str, 
+               cuda_num : str):
     
+    print("Getting Loaders")
+    test_loader, test_loader_vis, images_fp, labels_fp = getLoader(images_Dir=testing_images,
+                                                labels_Dir=testing_labels)
+    print("Done with Loaders")
+    test_loader_len = torch.tensor(len(test_loader))
+    
+    tor_device = torch.device(f"cuda:{cuda_num}")
     smp_model_dirpath = os.path.join(smp_inputs_path, smp_model)
     
     ERROR_path = os.path.join(smp_model_dirpath, "ERROR")
-    if os.path.exists("ERROR_path"):
-        continue
+    if os.path.exists(ERROR_path):
+        return
     
     modelpth_path = os.path.join(smp_model_dirpath, "model.pth")
     if not os.path.exists(modelpth_path):
-        continue
+        return
     model = torch.load(modelpth_path, map_location=tor_device)
     
-    # with tempfile.TemporaryDirectory() as temp_dir:
-    temp_dir = "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/psuedotemp"
-        
-    pr_collection = os.path.join(temp_dir, "predictions") # this is where images get saved to
-    gt_collection = os.path.join(temp_dir, "groundtruths")
-    output_path = os.path.join(smp_outputs_path, smp_model)
     
-    if not os.path.exists(pr_collection):
-        os.mkdir(pr_collection)
-    if not os.path.exists(gt_collection):
-        os.mkdir(gt_collection)
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-    
-    img_count = 0
-    for im, gt in test_loader:
-        im_tensor = torch.from_numpy(im).to(tor_device).unsqueeze(0)
-        pr_tensor = model.predict(im_tensor)
+    with tempfile.TemporaryDirectory() as temp_dir:
         
-        gt = gt.squeeze()[..., None, None, None]
-        pr = pr_tensor.cpu().detach().numpy().squeeze()[..., None, None, None]
+        pr_collection = os.path.join(temp_dir, "predictions") # this is where images get saved to
+        gt_collection = os.path.join(temp_dir, "groundtruths")
+        output_path = os.path.join(smp_outputs_path, smp_model)
         
-        assert gt.shape == pr.shape
+        if not os.path.exists(pr_collection):
+            os.mkdir(pr_collection)
+        if not os.path.exists(gt_collection):
+            os.mkdir(gt_collection)
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
         
-        filename = f"Image_{img_count}.tif"
-        pr_filename = os.path.join(pr_collection, filename)
-        gt_filename = os.path.join(gt_collection, filename)
-        
-        with BioWriter(pr_filename, Y=pr.shape[0],
-                                    X=pr.shape[1],
-                                    Z=1,
-                                    C=1,
-                                    T=1,
-                                    dtype=pr.dtype) as bw_pr:
-            bw_pr[:] = pr
+        img_count = 0
+        for im, gt in test_loader:
+            im_tensor = torch.from_numpy(im).to(tor_device).unsqueeze(0)
+            pr_tensor = model.predict(im_tensor)
             
-        with BioWriter(gt_filename, Y=gt.shape[0],
-                                    X=gt.shape[1],
-                                    Z=1,
-                                    C=1,
-                                    T=1,
-                                    dtype=gt.dtype) as bw_gt:
-            bw_gt[:] = gt
+            gt = gt.squeeze()[..., None, None, None]
+            pr = pr_tensor.cpu().detach().numpy().squeeze()[..., None, None, None]
             
-        img_count = img_count + 1
+            assert gt.shape == pr.shape
+            
+            filename = f"Image_{img_count}.tif"
+            pr_filename = os.path.join(pr_collection, filename)
+            gt_filename = os.path.join(gt_collection, filename)
+            
+            with BioWriter(pr_filename, Y=pr.shape[0],
+                                        X=pr.shape[1],
+                                        Z=1,
+                                        C=1,
+                                        T=1,
+                                        dtype=pr.dtype) as bw_pr:
+                bw_pr[:] = pr
+                
+            with BioWriter(gt_filename, Y=gt.shape[0],
+                                        X=gt.shape[1],
+                                        Z=1,
+                                        C=1,
+                                        T=1,
+                                        dtype=gt.dtype) as bw_gt:
+                bw_gt[:] = gt
+                
+            img_count = img_count + 1
         
+        queue.put(cuda_num)
         python_command = "python /home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/polus-plugins/features/polus-pixelwise-evaluation-plugin/src/main.py" + \
                         f" --GTDir {gt_collection}" + \
                         f" --PredDir {pr_collection}" + \
                         f" --inputClasses 1" + \
-                        f" --individualStats True" + \
+                        f" --individualStats False" + \
                         f" --totalStats True" + \
                         f" --outDir {output_path}"
-                        
+                            
         print(python_command)
-        
+            
         logfile = open(os.path.join(output_path, "logs.log"), 'a')
         subprocess.call(python_command, shell=True, stdout=logfile, stderr=logfile)
-                            
-        
+                                
             
+NUM_GPUS = len(available_gpus)
+NUM_PROCESSES = len(smp_inputs_list)
+PROC_PER_GPU = int(np.ceil(NUM_PROCESSES/NUM_GPUS))
+
+
+for gpu_ids in (range(8)):
+    queue.put(str(gpu_ids))
             
-            
-            
-            
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            # i = 0
+    for smp_input in smp_inputs_list:
+        executor.submit(evaluation, smp_input, queue.get())
             
             
             
