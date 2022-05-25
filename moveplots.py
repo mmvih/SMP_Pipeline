3c7c28ac323a45294bfb1ad88968b8e8dbd7b083
mmvih <mmvihani@gmail.com>
2022-05-25 12:57:48 -0600

    added time json and executor.submit

--- moveplots.py
+++ moveplots.py
@@ -13,6 +13,7 @@ from itertools import cycle, islice
 from itertools import repeat
 
 from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Queue
 
 import torch
 import torchvision
@@ -35,19 +36,27 @@ from src.utils import LOSSES
 
 import math
 
-smp_inputs_path =  "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/dummy_output_SMP"
-smp_outputs_path = "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/dummy_output_SMP_thresanalysis"
+smp_inputs_path =  "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/output_SMP"
+smp_outputs_path = "/home/vihanimm/SegmentationModelToolkit/workdir/SMP_Pipeline/output_SMP_thresanalysis"
+if not os.path.exists(smp_outputs_path): os.mkdir(smp_outputs_path)
 
 testing_images = "/home/vihanimm/SegmentationModelToolkit/Data/tif_data/nuclear/test/image/"
 testing_labels = "/home/vihanimm/SegmentationModelToolkit/Data/tif_data/nuclear/test/groundtruth_centerbinary_2pixelsmaller/"
 
 smp_inputs_list = os.listdir(smp_inputs_path)
+print(len(smp_inputs_list))
+for smp_input in smp_inputs_list:
+    smp_input_model_dirpath = os.path.join(smp_inputs_path, smp_input)
+    smp_input_model_path = os.path.join(os.path.join(smp_input_model_dirpath, "model.pth"))
+    smp_input_ERROR_path = os.path.join(os.path.join(smp_input_model_dirpath, "ERROR"))
+    if not os.path.exists(smp_input_model_path):
+        smp_inputs_list.remove(smp_input)
 
 def getLoader(images_Dir, 
               labels_Dir):
         
     # nuclear_test_61{x}.tif for only ten images
-    my_fp = "nuclear_test_60{x}.tif"
+    my_fp = ".*"
     images_fp = FilePattern(images_Dir, my_fp)
     labels_fp = FilePattern(labels_Dir, my_fp)
     
@@ -124,6 +133,7 @@ metrics.append(metric_loss)
 
 avg_model_metric_comparison = {metric.__name__ : {} for metric in metrics}
 std_model_metric_comparison = {metric.__name__ : {} for metric in metrics}
+model_times                 = {}
 
 fig, ax = plt.subplots(1, 3, figsize = (12, 4))
 
@@ -144,135 +154,129 @@ def forloop(smp_input_file,
             smp_outputs_path,
             device):
     
-    print(smp_input_file, flush=True)
-    starttime = time.time()
-    smp_input_path = os.path.join(smp_inputs_path, smp_input_file)
-    plot_path = os.path.join(smp_input_path, "Logs.jpg")
-    model_path = os.path.join(smp_input_path, "model.pth")
-    config_path = os.path.join(smp_input_path, "config.json")
-    ERROR_path = os.path.join(smp_input_path, "ERROR")
-    
-    if os.path.exists(ERROR_path):
-        print("ERROR")
-        return
+    try:
+        print(smp_input_file, flush=True)
+        
+        smp_input_path = os.path.join(smp_inputs_path, smp_input_file)
+        plot_path = os.path.join(smp_input_path, "Logs.jpg")
+        model_path = os.path.join(smp_input_path, "model.pth")
+        config_path = os.path.join(smp_input_path, "config.json")
+        ERROR_path = os.path.join(smp_input_path, "ERROR")
+        
+        # if os.path.exists(ERROR_path):
+        #     print("ERROR")
+        #     queue.put(device)
+        #     return
 
-    smp_output_path = os.path.join(smp_outputs_path, smp_input_file)
-    if not os.path.exists(smp_output_path):
-        os.mkdir(smp_output_path)
+        smp_output_path = os.path.join(smp_outputs_path, smp_input_file)
+        if not os.path.exists(smp_output_path):
+            os.mkdir(smp_output_path)
         
-    # if os.path.exists(plot_path):
-    #     shutil.copy(plot_path, os.path.join(smp_output_path, "scores.jpg"))
-        
-    if os.path.exists(model_path) and os.path.exists(config_path):
-        
-        model = torch.load(model_path,map_location=device)
-        
-        configObj     = open(config_path, 'r')
-        configDict    = json.load(configObj)
+        starttime = time.time()  
+        if os.path.exists(model_path) and os.path.exists(config_path):
+            
+            model = torch.load(model_path,map_location=device)
+            
+            configObj     = open(config_path, 'r')
+            configDict    = json.load(configObj)
 
-
-        metric_outputs = {}
-        best_metric_outputs  = {}
-        worst_metric_outputs = {}      
-        for metric in metrics:
-            metric_outputs[metric.__name__] = {'model': smp_input_file, 'metric': metric.__name__, 'avg': torch.tensor(0).to(device).float(), 'maxi': torch.tensor(0), 'mini': torch.tensor(1), 
-                                                'stddev' : torch.tensor(0), 'all': example_names.copy(), 'time_allmetrics': 0}
-            best_metric_outputs[metric.__name__] = {}
-            worst_metric_outputs[metric.__name__] = {}
-        
-        output_prediction_dir = os.path.join(smp_output_path, "Predictions")
-        if not output_prediction_dir:
-            os.mkdir(output_prediction_dir)
-        
-        i = 0
-        for test in test_loader:
-            test0 = test[0]
-            test1 = test[1]
-            xtensor = torch.from_numpy(test0).to(device).unsqueeze(0)
-            ytensor = torch.from_numpy(test1).to(device).unsqueeze(0)
-            ztensor = torch.from_numpy(test_loader_vis[i][0]).to(device).unsqueeze(0)
-            pr_mask = model.predict(xtensor)
-            pr_mask[pr_mask >= .50] = 1
-            pr_mask[pr_mask < .50] = 0 
-
+            metric_outputs = {}
+            best_metric_outputs  = {}
+            worst_metric_outputs = {}      
             for metric in metrics:
-                try:
-                    metric_value = (METRICS[metric.__name__].forward(self=metric, y_pr=pr_mask, y_gt=ytensor))
-                except:
-                    metric_value = (LOSSES[metric.__name__].forward(self=metric, y_pred=pr_mask, y_true=ytensor))
-                
-                metric_outputs[metric.__name__]['avg'] += torch.divide(metric_value, test_loader_len)
-                metric_outputs[metric.__name__]['all'][example_names_list[i]] = metric_value.item()
-                metric_outputs[metric.__name__]['mini'] = torch.minimum(metric_value, metric_outputs[metric.__name__]['mini'])
-                metric_outputs[metric.__name__]['maxi'] = torch.maximum(metric_value, metric_outputs[metric.__name__]['maxi'])
+                metric_outputs[metric.__name__] = {'model': smp_input_file, 'metric': metric.__name__, 'avg': torch.tensor(0).to(device).float(), 'maxi': torch.tensor(0), 'mini': torch.tensor(1), 
+                                                    'stddev' : torch.tensor(0), 'all': example_names.copy(), 'time_allmetrics': 0}
+                best_metric_outputs[metric.__name__] = {}
+                worst_metric_outputs[metric.__name__] = {}
             
-                output_prediction_file = os.path.join(output_prediction_dir, example_names_list[i])
-                with BioWriter(output_prediction_file, X=256, Y=256, Z=1, C=1, T=1, dtype=np.float32) as bw:
-                    bw[:] = np.squeeze(pr_mask.cpu().numpy())
-            i += 1 
+            i = 0
+            for test in test_loader:
+                test0 = test[0]
+                test1 = test[1]
+                xtensor = torch.from_numpy(test0).to(device).unsqueeze(0)
+                ytensor = torch.from_numpy(test1).to(device).unsqueeze(0)
+                ztensor = torch.from_numpy(test_loader_vis[i][0]).to(device).unsqueeze(0)
+                pr_mask = model.predict(xtensor)
+                pr_mask[pr_mask >= .50] = 1
+                pr_mask[pr_mask < .50] = 0 
+
+                for metric in metrics:
+                    try:
+                        metric_value = (METRICS[metric.__name__].forward(self=metric, y_pr=pr_mask, y_gt=ytensor))
+                    except:
+                        metric_value = (LOSSES[metric.__name__].forward(self=metric, y_pred=pr_mask, y_true=ytensor))
                     
-                # if i < 2:
-                #     best_metric_outputs[metric.__name__][float(metric_value)] = {"image"       : torch.squeeze(ztensor),
-                #                                                                  "groundtruth" : torch.squeeze(ytensor),
-                #                                                                  "prediction"  : torch.squeeze(pr_mask)}
+                    metric_outputs[metric.__name__]['avg'] += torch.divide(metric_value, test_loader_len)
+                    metric_outputs[metric.__name__]['all'][example_names_list[i]] = metric_value.item()
+                    metric_outputs[metric.__name__]['mini'] = torch.minimum(metric_value, metric_outputs[metric.__name__]['mini'])
+                    metric_outputs[metric.__name__]['maxi'] = torch.maximum(metric_value, metric_outputs[metric.__name__]['maxi'])
                 
-                #     worst_metric_outputs[metric.__name__][float(metric_value)] = {"image"      : torch.squeeze(ztensor),
-                #                                                                   "groundtruth" : torch.squeeze(ytensor),
-                #                                                                   "prediction"  : torch.squeeze(pr_mask)}
-                # else:
-                #     worstof_bestvalues = min(best_metric_outputs[metric.__name__].keys())
-                #     bestof_worstvalues = max(worst_metric_outputs[metric.__name__].keys())
-
-                #     if metric_value > worstof_bestvalues:
-                #         del best_metric_outputs[metric.__name__][worstof_bestvalues]
-                #         best_metric_outputs[metric.__name__][float(metric_value)] = {"image"       : torch.squeeze(ztensor),
-                #                                                                      "groundtruth" : torch.squeeze(ytensor),
-                #                                                                      "prediction"  : torch.squeeze(pr_mask)}
-                    
-                #     if metric_value < bestof_worstvalues:
-                #         del worst_metric_outputs[metric.__name__][bestof_worstvalues]
-                #         worst_metric_outputs[metric.__name__][float(metric_value)] = {"image"       : torch.squeeze(ztensor),
-                #                                                                      "groundtruth" : torch.squeeze(ytensor),
-                #                                                                      "prediction"  : torch.squeeze(pr_mask)}
-             
-        
-        metric_names = [metric.__name__ for metric in metrics]
-        metric_values = [metric_outputs[metric]['avg'].item() for metric in metric_names]
-
-        totaltime = time.time()-starttime
-        
-        for metric in metrics:
-            # getPlots(best_metric_outputs[metric.__name__], metric_name=metric.__name__, smp_output_path, type="best", fig=fig, ax=ax)
-            # getPlots(worst_metric_outputs[metric.__name__], metric_name=metric.__name__, smp_output_path, type="worst", fig=fig, ax=ax)
-            metric_outputs[metric.__name__]['stddev'] = np.std(list(metric_outputs[metric.__name__]['all'].values()))
-            metric_outputs[metric.__name__]['avg'] = metric_outputs[metric.__name__]['avg'].item()
-            metric_outputs[metric.__name__]['mini'] = metric_outputs[metric.__name__]['mini'].item()
-            metric_outputs[metric.__name__]['maxi'] = metric_outputs[metric.__name__]['maxi'].item()
-            metric_outputs[metric.__name__]['all']  = {k: v for k, v in sorted(metric_outputs[metric.__name__]['all'].items(), 
-                                                                    key=lambda item: item[1])}
-            metric_outputs[metric.__name__]['time_allmetrics'] = totaltime
+                i += 1 
             
-            avg_model_metric_comparison[metric.__name__][smp_input_file] = metric_outputs[metric.__name__]['avg']
-            std_model_metric_comparison[metric.__name__][smp_input_file] = metric_outputs[metric.__name__]['stddev']
-            metrics_json = os.path.join(smp_output_path, f"metrics_{metric.__name__}.json")
-            with open(metrics_json, 'w') as metric_json:
-                json.dump(metric_outputs[metric.__name__], metric_json, indent=4)
+            metric_names = [metric.__name__ for metric in metrics]
+            metric_values = [metric_outputs[metric]['avg'].item() for metric in metric_names]
 
-        print("TOTAL TIME: ", totaltime, flush=True)
-        print(" ")
+            totaltime = time.time()-starttime
+            model_times[smp_input_file] = totaltime
+            
+            for metric in metrics:
+                # getPlots(best_metric_outputs[metric.__name__], metric_name=metric.__name__, smp_output_path, type="best", fig=fig, ax=ax)
+                # getPlots(worst_metric_outputs[metric.__name__], metric_name=metric.__name__, smp_output_path, type="worst", fig=fig, ax=ax)
+                metric_outputs[metric.__name__]['stddev'] = np.std(list(metric_outputs[metric.__name__]['all'].values()))
+                metric_outputs[metric.__name__]['avg'] = metric_outputs[metric.__name__]['avg'].item()
+                metric_outputs[metric.__name__]['mini'] = metric_outputs[metric.__name__]['mini'].item()
+                metric_outputs[metric.__name__]['maxi'] = metric_outputs[metric.__name__]['maxi'].item()
+                metric_outputs[metric.__name__]['all']  = {k: v for k, v in sorted(metric_outputs[metric.__name__]['all'].items(), 
+                                                                        key=lambda item: item[1])}
+                metric_outputs[metric.__name__]['time_allmetrics'] = totaltime
+                
+                avg_model_metric_comparison[metric.__name__][smp_input_file] = metric_outputs[metric.__name__]['avg']
+                std_model_metric_comparison[metric.__name__][smp_input_file] = metric_outputs[metric.__name__]['stddev']
+                metrics_json = os.path.join(smp_output_path, f"metrics_{metric.__name__}.json")
+                with open(metrics_json, 'w') as metric_json:
+                    json.dump(metric_outputs[metric.__name__], metric_json, indent=4)
+
+            print("TOTAL TIME: ", totaltime, flush=True)
+            print(" ")
+    except:
+        print(f"{smp_input_file} threw error")
+    finally:
+        queue.put(device)
 
 gpu_list = [0, 1, 2, 3, 5, 6, 7]
 gpu_list = list(islice(cycle(gpu_list), len(smp_inputs_list)))
 gpu_list = [torch.device(f"cuda:{gpu}") for gpu in gpu_list]
 
-with ThreadPoolExecutor(max_workers=8) as executor:
-    executor.map(forloop, smp_inputs_list, repeat(smp_inputs_path), repeat(smp_outputs_path), gpu_list)
-# for smp_input_file, gpu in zip(smp_inputs_list, gpu_list):
+queue = Queue()
+for i in range(8):
+    queue.put(torch.device(f"cuda:{i}"))
+
+counter = 0
+sleeping_in = 0
+with ThreadPoolExecutor(max_workers=20) as executor:
+    for smp_input in smp_inputs_list:
+        
+        smp_input_path = os.path.join(smp_inputs_path, smp_input)
+        plot_path = os.path.join(smp_input_path, "Logs.jpg")
+        model_path = os.path.join(smp_input_path, "model.pth")
+        config_path = os.path.join(smp_input_path, "config.json")
+        ERROR_path = os.path.join(smp_input_path, "ERROR")
+        
+        if os.path.exists(ERROR_path):
+            continue
+        
+        if not os.path.exists(model_path):
+            continue
     
-#     forloop(smp_input_file=smp_input_file,
-#             smp_inputs_path=smp_inputs_path,
-#             smp_outputs_path=smp_outputs_path,
-#             device=torch.device(f"cuda:{gpu}"))
+        while queue.empty():
+            sleeping_in += 1
+            time.sleep(30)
+        
+        if not queue.empty():
+            executor.submit(forloop, smp_input, smp_inputs_path, smp_outputs_path, queue.get())
+
+    # executor.map(forloop, smp_inputs_list, repeat(smp_inputs_path), repeat(smp_outputs_path), gpu_list)
+
     
 for metric in metrics:    
     model_json_metric_path = os.path.join(smp_outputs_path, metric.__name__ + "_models.json")
@@ -281,6 +285,10 @@ for metric in metrics:
     print(avg_model_metric_comparison[metric.__name__])
     with open(model_json_metric_path, 'w') as config_file:
         json.dump(avg_model_metric_comparison[metric.__name__], config_file, indent=4)
-        
+model_json_time_path = os.path.join(smp_outputs_path, "time_models.json")
+model_times = {k: v for k, v in sorted(model_times.items(), 
+                                        key=lambda item: item[1])}
+with open(model_json_time_path, 'w') as time_file:
+    json.dump(model_times, time_file, indent=4)
         
         
