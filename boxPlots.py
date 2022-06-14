954204be8e0e7b1f701dc067efe289df3bccb0c8
Madhuri Vihani <43178933+mmvih@users.noreply.github.com>
2022-06-14 11:07:52 -0400

    Update boxPlots.py

--- boxPlots.py
+++ boxPlots.py
@@ -21,7 +21,7 @@ def append_to_dataframe(eval_dirpath, curr_smp_metric, metrics):
     total_stats_result_path = os.path.join(eval_dirpath, "total_stats_result.csv")
     logger.debug(f"Total Stats Path : {os.path.abspath(total_stats_result_path)}")
     if not os.path.exists(total_stats_result_path):
-        return 0
+        return 0, 0, 0
         
     result_path = os.path.join(eval_dirpath, "result.csv")
     logger.debug(f"Result Path : {os.path.abspath(result_path)}")
@@ -37,13 +37,19 @@ def append_to_dataframe(eval_dirpath, curr_smp_metric, metrics):
         else:
             metrics[column][curr_smp_metric] = list(result_df[column])
     
-    result_df.loc[f'{curr_smp_metric}_mean'] = result_df[0:1249].mean()
-    result_df.loc[f'{curr_smp_metric}_std']  = result_df[0:1249].std()
+    result_df_avg = result_df[0:1249].mean()
+    result_df_std = result_df[0:1249].std()
+    
+    result_df.loc[f'{curr_smp_metric}_mean'] = result_df_avg
+    result_df.loc[f'{curr_smp_metric}_std']  = result_df_std
+    
+    result_df_avg = result_df_avg.rename(curr_smp_metric)
+    result_df_std = result_df_std.rename(curr_smp_metric)
             
     result_df['Image_Name'] = result_df.index
     result_df = result_df.loc[[f'{curr_smp_metric}_mean', f'{curr_smp_metric}_std']]
         
-    return result_df
+    return result_df, result_df_avg, result_df_std
 
 def create_boxplots(dataframe, output_directory, show_howmany, metrics):
     
@@ -72,9 +78,6 @@ def create_boxplots(dataframe, output_directory, show_howmany, metrics):
         model_data_merged = pd.merge(model_data_avg, model_data_std, on="Image_Name")
         model_data_merged = model_data_merged.sort_values(by=[columnName+"_x"], ignore_index=True)
 
-        columnName_avg = columnName + "_x"
-        columnName_std = columnName + "_y"
-
         randgraph_idx = np.sort(np.random.randint(show_howmany+1, 
                                 len(model_data_avg)-show_howmany+1, 
                                 show_howmany).astype('int'))
@@ -82,8 +85,6 @@ def create_boxplots(dataframe, output_directory, show_howmany, metrics):
         lowest_models  = list(model_data_merged["Image_Name"].iloc[0:show_howmany])
         middle_models  = list(model_data_merged["Image_Name"].iloc[randgraph_idx])
         highest_models = list(model_data_merged["Image_Name"].iloc[-show_howmany:])
-        
-        print(lowest_models)
         
         lowest_models_values  = [metrics[columnName][lowest_model] for lowest_model in lowest_models]
         highest_models_values = [metrics[columnName][lowest_model] for lowest_model in highest_models]
@@ -105,7 +106,6 @@ def create_boxplots(dataframe, output_directory, show_howmany, metrics):
         logger.debug(f"Saved Boxplot - {output_boxplot_path}")
         
         plt.close()
-        del box
 
 def main():
     
@@ -133,8 +133,6 @@ def main():
     
     show_howmany = min(num_models//3, 50)
     
-    counter = 0
-    
     # intialize data for summarizing
     pixel_metrics_dataframe = pd.DataFrame()
     cell_metrics_dataframe  = pd.DataFrame()
@@ -146,6 +144,13 @@ def main():
                     f"to show the top {show_howmany}, middle {show_howmany}, " + \
                         f"and bottom {show_howmany} models")
 
+    pixel_metrics_dataframe_avg = pd.DataFrame()
+    pixel_metrics_dataframe_std = pd.DataFrame()
+    
+    cell_metrics_dataframe_avg = pd.DataFrame()
+    cell_metrics_dataframe_std = pd.DataFrame()
+    
+    counter = 0
     for curr_smp_metric in input_metrics_list:
         
         counter += 1
@@ -157,14 +162,38 @@ def main():
         pixeleval_dirpath = os.path.join(input_metric_dirpath, "pixeleval")
         celleval_dirpath  = os.path.join(input_metric_dirpath, "celleval")
         
-        pixel_metric_dataframe = append_to_dataframe(pixeleval_dirpath, curr_smp_metric, pixel_metrics)
-        cell_metric_dataframe  = append_to_dataframe(celleval_dirpath, curr_smp_metric, cell_metrics)
-        
+        pixel_metric_dataframe, pixel_metric_dataframe_avg, pixel_metric_dataframe_std = \
+            append_to_dataframe(pixeleval_dirpath, curr_smp_metric, pixel_metrics)
+        cell_metric_dataframe, cell_metric_dataframe_avg, cell_metric_dataframe_std = \
+            append_to_dataframe(celleval_dirpath, curr_smp_metric, cell_metrics)
+
         if not isinstance(pixel_metric_dataframe, int):
             pixel_metrics_dataframe = pixel_metrics_dataframe.append(pixel_metric_dataframe, ignore_index=True)
-        if not isinstance(pixel_metric_dataframe, int):
+            pixel_metrics_dataframe_avg = pixel_metrics_dataframe_avg.append(pixel_metric_dataframe_avg)
+            pixel_metrics_dataframe_std = pixel_metrics_dataframe_std.append(pixel_metric_dataframe_std)
+        
+        if not isinstance(cell_metric_dataframe, int):
             cell_metrics_dataframe  = cell_metrics_dataframe.append(cell_metric_dataframe, ignore_index=True)
+            cell_metrics_dataframe_avg = cell_metrics_dataframe_avg.append(cell_metric_dataframe_avg)
+            cell_metrics_dataframe_std = cell_metrics_dataframe_std.append(cell_metric_dataframe_std)
+
+    # new column names for the pixel and cell dataframes so that they can be combined and saved
+    newpixel_columnNames = {column : f"{column}_pixel" for column in pixel_metrics_dataframe_avg.columns}
+    newcell_columnNames = {column : f"{column}_cell" for column in cell_metrics_dataframe_std.columns}
+
+    # saving the average data
+    pixel_metrics_dataframe_avg.to_csv(os.path.join(output_boxplots_dirpath, "pixels_avg.csv"), index=True)
+    cell_metrics_dataframe_avg.to_csv(os.path.join(output_boxplots_dirpath, "cells_avg.csv"), index=True)
+    pd.concat([pixel_metrics_dataframe_avg.rename(columns=newpixel_columnNames), 
+               cell_metrics_dataframe_avg.rename(columns=newcell_columnNames)],
+               axis=1).to_csv(os.path.join(output_boxplots_dirpath, "combined_avg.csv"), index=True)
     
+    # saving the standard deviation data
+    pixel_metrics_dataframe_std.to_csv(os.path.join(output_boxplots_dirpath, "pixels_std.csv"), index=True)
+    cell_metrics_dataframe_std.to_csv(os.path.join(output_boxplots_dirpath, "cells_std.csv"), index=True)
+    pd.concat([pixel_metrics_dataframe_std.rename(columns=newpixel_columnNames), 
+               cell_metrics_dataframe_std.rename(columns=newcell_columnNames)],
+               axis=1).to_csv(os.path.join(output_boxplots_dirpath, "combined_std.csv"), index=True)
     
     output_pixel_boxplot_dirpath = os.path.join(output_boxplots_dirpath, "pixel_boxplots")
     output_cell_boxplot_dirpath  = os.path.join(output_boxplots_dirpath, "cell_boxplots")
@@ -181,8 +210,5 @@ def main():
     create_boxplots(pixel_metrics_dataframe, output_pixel_boxplot_dirpath, show_howmany, pixel_metrics)
     create_boxplots(cell_metrics_dataframe, output_cell_boxplot_dirpath, show_howmany, cell_metrics)
 
-
-        
-    
 main()
  
